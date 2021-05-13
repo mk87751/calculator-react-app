c1990732efd491a12149bdb6499902f0f12b55f5
mk87751 <mk87751@gmail.com>
2021-05-13 12:28:38 +0530

    Styles added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Caluclator from "./Caluclator";
+
+function App() {
+  return (
+    <div className="App">
+      This is Main
+      <Caluclator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Output.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Output.css";
+function Output() {
+  return <input className="output" />;
+}
+
+export default Output;
